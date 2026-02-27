88aa43cdb5d5d6569f86ca17cd331e5ae64305c5
OscarQuinter <devoscar1997@gmail.com>
2026-02-27 16:57:59 -0500

    Add Playwright setup with initial test configurations and example tests
    - Create Playwright workflow in GitHub Actions for CI
    - Add Playwright and Node.js dependencies in package.json
    - Implement Playwright configuration file
    - Add example tests for basic functionality and navigation
    - Include utility functions and class for testing
    - Update .gitignore to exclude Playwright-related files

--- tests/AutomationSandbox.spec.ts
+++ tests/AutomationSandbox.spec.ts
@@ -17,12 +17,18 @@ import { test, Browser, Page, expect } from '@playwright/test';
 
                 const botonIDDinamico = page.getByRole('button', { name: 'Hacé click para generar un ID dinámico y mostrar el elemento oculto' });
                 await botonIDDinamico.click({force: true});
+                await expect(page.getByText('OMG, aparezco después de 3 segundos')).toBeVisible();
+
                 // await botonIDDinamico.dblclick({});
                 // await botonIDDinamico.click({button: 'right', force: true});
                 // await botonIDDinamico.click({modifiers: ['Shift']});
                 // await botonIDDinamico.hover();
             })
 
+        
+        
+
+        
             await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
                 await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');
             })
@@ -33,11 +39,31 @@ import { test, Browser, Page, expect } from '@playwright/test';
              
         })
 
-        test('Puedo seleccionar checkboxes', async ({ page }) => {
+        test('Lleno un campo de texto en el sandbox', async ({ page }) => { 
+            await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');
+            })
+
+            await test.step('Puedo ingresar texto en el campo un Aburrido texto', async () => {
+                await expect(page.getByRole('textbox', { name: 'Un aburrido texto' }), 'El campo de texto no es editable').toBeEditable();
+                await page.getByRole('textbox', { name: 'Un aburrido texto' }).fill(textoAEscribir);
+                await expect(page.getByRole('textbox', { name: 'Un aburrido texto' }), 'El campo de texto no es editable').toHaveValue(textoAEscribir);
+
+
+            })
+        })
+
+        test('Puedo seleccionar y deseleccionar un checkboxes en el sandbox', async ({ page }) => {
                 await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');
 
                 await test.step('Puedo seleccionar el checkbox para pasta', async () => {
-                    await page.getByRole('checkbox', { name: 'Pasta 🍝' }).check
+                    await page.getByRole('checkbox', { name: 'Pasta 🍝' }).check();
+                    await expect(page.getByLabel( 'Pasta 🍝' )).toBeChecked();
+                })
+
+                await test.step('Puedo deseleccionar el checkbox pasta', async () => {
+                    await page.getByRole('checkbox', { name: 'Pasta 🍝' }).uncheck();
+                    await expect(page.getByLabel( 'Pasta 🍝' )).not.toBeChecked();
                 })
         })
 
@@ -49,18 +75,28 @@ import { test, Browser, Page, expect } from '@playwright/test';
 
             await test.step('Puedo seleccionar el radio Butoton para NO ', async () => {
                 await page.getByRole('radio', { name: 'No' }).check();
+                await expect(page.getByLabel( 'No' ), 'El radio button no ha sido seleccionado').toBeChecked();
             })
         })
 
         //Seleccion del dropdown
 
-        test ('Puedo seleccionar in item del dropdown', async ({ page }) => {
+        test ('Los items del dropdown son correctos', async ({ page }) => {
             await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
                 await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
             })
 
-            await test.step('Selecciono un deporte del dropdown ', async () => {
-                await page.getByLabel('Dropdown').selectOption('Fútbol');
+            await test.step('Vaido que la lista del dropdown contiene los deportes esperados ', async () => {
+                const deportes = ['Fútbol', 'Tennis', 'Basketball', 'Rugby'];
+                
+                for (let opcion of deportes) {
+                    const element = await page.$(`select#formBasicSelect > option:is(:text("${opcion}"))`);
+                    if (element) {
+                        console.log(`La opción "${opcion}" está presente.`);
+                    }else {
+                        throw new Error(`La opción "${opcion}" no está presente.`);
+                    }
+                }
             })
         })
 
@@ -77,28 +113,108 @@ import { test, Browser, Page, expect } from '@playwright/test';
         })
 
 
+        //Tabla estatica
+
+        test ('Valido la columna Nombres de la tabla estatica', async ({ page }) => {
+            await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
+            })
+
+            await test.step('Puedo validar los elementos para la columna Nombre de la tabla estatica ', async () => {
+                const valoresColumnaNombres = await page.$$eval('h2:has-text("Tabla estática") + table tbody tr td:nth-child(2)', elements => elements.map(element => element.textContent));
+                const nombresEsperados = ['Messi', 'Ronaldo', 'Mbappe'];
+                expect(valoresColumnaNombres).toEqual(nombresEsperados);
+            })
+        })
+
+
+        //Tabla dinámica
+
+
+        test ('Valido que todos los valores cambian en la tabla dinamica luego de un reload', async ({ page }) => {
+            await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
+            })
+
+            await test.step('Valido que los valores cambiaron al hacer un reload a la web ', async () => {
+
+                //creamos un array con los valores de la tabla dinámica
+                const valoresTablaDinamica = await page.$$eval('h2:has-text("Tabla dinámica") + table tbody tr td', elements => elements.map(element => element.textContent));
+                console.log(valoresTablaDinamica);
+                
+                // Hago una recarga de la pagina para que se generen nuevos valores en la tabla dinámica
+                await page.reload();
+
+                // Creamos un segundo array con los valores de la tabla dinámica luego del reload
+                const valoresPostReload = await page.$$eval('h2:has-text("Tabla dinámica") + table tbody tr td', elements => elements.map(element => element.textContent));
+                console.log(valoresPostReload);
+                
+                // Validamos que los valores sean diferentes
+                expect(valoresTablaDinamica).not.toEqual(valoresPostReload);
+            })
+        })
+
+
+        test('Ejemplo de Soft Assertions', async ({ page }) => {
+
+            await test.step('Dado que navego al Sandbox de Automation de Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');
+            });
+
+            await test.step('Valido que todos los elementos de los checkboxes son los correctos', async () => {
+
+                await expect.soft(page.getByText('Pizza 🍕')).toBeVisible();
+                await expect.soft(page.getByText('Hamburguesaa 🍔'), 'No se encontro el elemento de Hamburguesa  🍔').toBeVisible();
+                await expect.soft(page.getByText('Pasta 🍝')).toBeVisible();
+                await expect.soft(page.getByText('Helado0 🍧'), 'No se encontro el elemento de Helado 🍧').toBeVisible();
+                await expect.soft(page.getByText('Torta 🍰')).toBeVisible();
+            });
+
+        });
+
+
+        test ('Validando dentro de un popup', async ({ page }) => {
+
+            const popupPromise = page.waitForEvent('popup');
+            await test.step('dado que navego al sandbox', async () => {
+                await page.goto('')
+            })
+
+            await test.step('Cuando hago click en el botón popup', async () => {
+                await page.getByRole('button', { name: 'Mostrar popup' }).click();
+            })
+
+            await test.step('Puedo validar un elemento dentro de un popup', async () => {
+                const popup = await popupPromise;
+                await popup.waitForLoadState();
+                await expect(popup.getByText('¿Viste? ¡Apareció un popup!')).toHaveText('¿Viste? ¡Apareció un pop-ceup!');
+                await page.getByRole('button', { name: 'Cerrar' }).click();
+            })
+
+
+        })
         //Subir archivos
 
-        test ('Puedo subir archivos a Automation Sandbox', async ({ page }) => {
-            await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
-                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
-            })
+        // test ('Puedo subir archivos a Automation Sandbox', async ({ page }) => {
+        //     await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
+        //         await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
+        //     })
 
-            await test.step('Agrego archivos para ser subidos', async () => {
-                await page.getByLabel('Upload file').setInputFiles(['pathAlArchivo.pdf', 'invoice1.pdf', 'invoice2.pdf']);
-                await page.getByLabel('Upload file').setInputFiles([]);
-            })
-        })
+        //     await test.step('Agrego archivos para ser subidos', async () => {
+        //         await page.getByLabel('Upload file').setInputFiles(['pathAlArchivo.pdf', 'invoice1.pdf', 'invoice2.pdf']);
+        //         await page.getByLabel('Upload file').setInputFiles([]);
+        //     })
+        // })
 
-        test ('Puedo hacer un Drag and Drop de elementos en Automation Sandbox', async ({ page }) => {
-            await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
-                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
-            })
+        // test ('Puedo hacer un Drag and Drop de elementos en Automation Sandbox', async ({ page }) => {
+        //     await test.step('Dado que navego al sandbox de information Free Range Testers', async () => {
+        //         await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/');  
+        //     })
 
-            await test.step('Agrego archivos para ser subidos', async () => {
-                await page.getByTestId('DragFrom').dragTo(page.getByTestId('DragTo'));
-            })
-        })
+        //     await test.step('Agrego archivos para ser subidos', async () => {
+        //         await page.getByTestId('DragFrom').dragTo(page.getByTestId('DragTo'));
+        //     })
+        // })
     })
 
 
